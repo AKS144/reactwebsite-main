b1c188ab566408f940045a0c920e83f44b5f6780
AKS145 <abhi145k@gmail.com>
2023-05-07 12:24:26 +0530

    check

--- src/components/inc/Slider.js
+++ src/components/inc/Slider.js
@@ -9,27 +9,15 @@ function Slider(){
                 <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
                 <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
             </div>
-            <div className="carousel-inner">
+            <div className="carousel-inner hh">
                 <div className="carousel-item active">
-                <img src={Slider1} className="d-block w-100" alt="..." />
-                <div className="carousel-caption d-none d-md-block">
-                    <h5>First slide label</h5>
-                    <p>Some representative placeholder content for the first slide.</p>
-                </div>
+                    <img src={Slider1} className="d-block w-100" alt="..." />
                 </div>
                 <div className="carousel-item">
-                <img src={Slider1} className="d-block w-100" alt="..." />
-                <div className="carousel-caption d-none d-md-block">
-                    <h5>Second slide label</h5>
-                    <p>Some representative placeholder content for the second slide.</p>
-                </div>
+                    <img src={Slider1} className="d-block w-100" alt="..." />
                 </div>
                 <div className="carousel-item">
-                <img src={Slider1} className="d-block w-100" alt="..." />
-                <div className="carousel-caption d-none d-md-block">
-                    <h5>Third slide label</h5>
-                    <p>Some representative placeholder content for the third slide.</p>
-                </div>
+                    <img src={Slider1} className="d-block w-100" alt="..." />
                 </div>
             </div>
             <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
